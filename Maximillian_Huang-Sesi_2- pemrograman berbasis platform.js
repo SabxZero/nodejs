6679ef02435405d5c2c36fadf85f8142a2b3f60b
Maximillian.H <114460381+SabxZero@users.noreply.github.com>
2023-10-01 12:24:08 +0700

    Add files via upload

---
+++ Maximillian_Huang-Sesi_2- pemrograman berbasis platform.js
@@ -0,0 +1,11 @@
+//variable
+var firstName = "Maximillian"
+var lastName = "Huang"
+var age = 19
+var isMarried = false
+
+//output
+console.log("nama depan saya = " + firstName)
+console.log("nama belakang saya = " + lastName)
+console.log("umurku = " + age)
+console.log("status menikah = " + isMarried)
